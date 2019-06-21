3918689a7e6ee710053530ed71bd9d8b9f6bbf6d
ThundaHorse <abeeeeekim@gmail.com>
2019-06-21 15:34:29 -0500

    configured CORS for VueJS

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,7 +5,7 @@ class Api::UsersController < ApplicationController
   end 
 
   def create
-    user = User.new(
+    @user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name], 
       email: params[:email],
@@ -15,10 +15,10 @@ class Api::UsersController < ApplicationController
       password_confirmation: params[:password_confirmation]
     )
 
-    if user.save
+    if @user.save
       render json: {message: 'User created successfully'}, status: :created
     else
-      render json: {errors: user.errors.full_messages}, status: :bad_request
+      render json: {errors: @user.errors.full_messages}, status: :bad_request
     end
   end
 end
