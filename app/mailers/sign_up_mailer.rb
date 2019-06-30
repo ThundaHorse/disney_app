37c03f71bcc5eae7dfecf12d10bb0837cf086e97
ThundaHorse <abeeeeekim@gmail.com>
2019-06-30 18:46:12 -0500

    completed action mailer functionality, starting send after user sign up

--- app/mailers/sign_up_mailer.rb
+++ app/mailers/sign_up_mailer.rb
@@ -7,8 +7,11 @@ class SignUpMailer < ApplicationMailer
   #
   def creation(user)
     @user = user 
+    attachments.inline['mickey.png'] = File.read('/Users/abrahamkim/Desktop/Disney_capstone/disney_vue_app/public/disney/mickey.png') 
     
+
     mail to: @user.email,
         subject: "Welcome to Disney Trip Planning!"
   end
+
 end
